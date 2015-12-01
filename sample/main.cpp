93bb007e8aba0c485cfbcbc25f18524f08b0d2f5
Vitalii Shkibtan <v.shkibtan@gmail.com>
2015-12-02 00:35:54 +0200

    Create project structure

---
+++ sample/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "libbmp.h"
+
+int main()
+{
+    std::cout << "Sample" << std::endl;
+
+    return 0;
+}
+
---
+++ tests/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "gtest/gtest.h"
+
+TEST(TestTest, Test)
+{
+    EXPECT_EQ(2, 2);
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
